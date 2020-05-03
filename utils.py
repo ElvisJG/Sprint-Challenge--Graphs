f4ac0adf6b7d37ee8447397ee3a117e570028f07
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-05-03 12:33:05 -0400

    formatting, IDE stuff, utils

---
+++ utils.py
@@ -0,0 +1,37 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def size(self):
+        return len(self.queue)
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def remove_item(self, index):
+        return self.stack.pop(index)
+
+
